275c7036c5318b76ab84c6f9bbdfc25dceb239e9
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-02-11 20:50:46 +0700

    feat: add function can give two strings s and t, return true if t is an anagram of s, and false otherwise.

--- index.js
+++ index.js
@@ -18,3 +18,27 @@ const findSingleOne = (nums) => {
 console.log(findSingleOne([2, 2, 1]));
 console.log(findSingleOne([4, 1, 2, 1, 2]));
 console.log(findSingleOne([1]));
+
+console.log("___________________________________________________________");
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+const isAnagram = (s, t) => {
+    const collectCharS = {}, collectCharT = {};
+
+    for (const sChar of s) {
+        collectCharS[sChar] = collectCharS[sChar] ? collectCharS[sChar] + 1 : 1;
+    }
+
+    for (const tChar of s) {
+        collectCharT[tChar] = collectCharT[tChar] ? collectCharT[tChar] + 1 : 1;
+    }
+
+    return JSON.stringify(collectCharS) === JSON.stringify(collectCharT);
+}
+
+console.log(isAnagram("anagram", "nagaram"));
+console.log(isAnagram("rat", "car"));
+
+console.log("___________________________________________________________");
